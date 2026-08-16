5e7c6b0fc457414eb0842743d50115b2f7df7258
Simon Baum <simon.g.baum@gmail.com>
2026-08-16 08:00:02 -0400

    Add user registration endpoint with validation

---
+++ server/user_app/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email','password']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'required': True, 'allow_blank': False},
+            'first_name': {'required': True, 'allow_blank': False},
+            'last_name': {'required': True, 'allow_blank': False},
+        }
+    
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with that email already exists.")
+        return value
+    
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- server/user_app/urls.py
+++ server/user_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .views import Register
+
 urlpatterns = [
-    
+    path('register/', Register.as_view()),
 ]
