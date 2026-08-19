74d3910c69e95eea7fe2e10fa092982b034d6e5d
Simon Baum <simon.g.baum@gmail.com>
2026-08-18 22:41:51 -0400

    18AUG2026: Add token refresh interceptor with retry guard to axios utility.

--- client/src/utilities.js
+++ client/src/utilities.js
@@ -5,4 +5,32 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (originalRequest.url === "users/refresh/") {
+      return Promise.reject(error);
+    }
+    if (error.response?.status !== 401) {
+      return Promise.reject(error);
+    }
+    if (originalRequest.alreadyRetried) {
+      return Promise.reject(error);
+    }
+
+    originalRequest.alreadyRetried = true;
+
+    try {
+      await api.post("users/refresh/");
+      return api(originalRequest);
+    } catch (refreshError) {
+      return Promise.reject(refreshError);
+    }
+  }
+);
+
 export default api;
