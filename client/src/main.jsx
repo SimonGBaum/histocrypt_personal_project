45537cf0346c1897441151967b65f7d945ad6618
Simon Baum <simon.g.baum@gmail.com>
2026-08-19 08:59:47 -0400

    19AUG2026: Remove StrictMode so effects fire once.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
@@ -6,7 +5,5 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
 )
