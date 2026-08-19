cd2f93ff0e91c2572969dd0372ca8e5ae086799d
Simon Baum <simon.g.baum@gmail.com>
2026-08-18 21:58:40 -0400

    18AUG2026: Add log out button and session clearing to home page.

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,12 +1,25 @@
 import { useOutletContext } from "react-router-dom";
+import api from "../utilities";
 
 export default function HomePage() {
-  const { user } = useOutletContext();
+  const { user, setUser } = useOutletContext();
+
+  const handleLogOut = async () => {
+    try {
+      await api.post("users/logout/");
+      setUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
       <p>{user.username}</p>
       <h2>Home Page</h2>
+      <button className="btn btn-secondary" onClick={handleLogOut}>
+        Log Out
+      </button>
     </>
   );
 }
